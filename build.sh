fa27a783df28aa83ed056571244b8eac30381a66
Shamim Mirzai <sham@mumsnet.com>
2019-06-16 19:56:40 +0100

    initial

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# For reference, make the image tag patch version number match the PHP patch version number
+docker build -t php-lambda-7-3:1.0.6 .
+docker tag php-lambda-7-3:1.0.6 mumsnet/php-lambda-7-3:1.0.6
+docker login --username=mumsnet
+docker push mumsnet/php-lambda-7-3:1.0.6
