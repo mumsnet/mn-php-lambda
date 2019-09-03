ff5d69a1018de7757e608d85a0abcfded58b9ea5
Shamim Mirzai <sham@mumsnet.com>
2019-09-03 14:12:55 +0100

    Use newer ssl and put it in /opt/openssl

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 # For reference, make the image tag patch version number match the PHP patch version number
-docker build -t php-lambda-7-3:1.0.6 -t php-lambda-7-3:latest .
-docker tag php-lambda-7-3:1.0.6 mumsnet/php-lambda-7-3:1.0.6
-docker tag php-lambda-7-3:latest mumsnet/php-lambda-7-3:latest
+docker build -t php-lambda:7.3 .
+docker tag php-lambda:7.3 mumsnet/php-lambda:7.3
 docker login --username=mumsnet
-docker push mumsnet/php-lambda-7-3:1.0.6
-docker push mumsnet/php-lambda-7-3:latest
+docker push mumsnet/php-lambda:7.3
